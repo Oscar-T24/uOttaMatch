a87b1414384d6cbe567c7f65d2e09b0ef057238c
Oscar-T24 <o.tesniere24@ejm.org>
2025-01-18 13:31:46 -0500

    fixed compatibility issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import CommonLayout from "./components/commonLayout/CommonLayout";
 import Main from "./components/main/Main";
 import Auth from "./components/auth/auth";
 import Map from "./components/maps/Map";
+import TestAPIConnection from "./components/TestAPIConnection";
 
 const Routes = () => {
   const element = useRoutes([
@@ -18,6 +19,18 @@ const Routes = () => {
               <Main />
               <Map />
             </Auth>
+          ),
+        },
+      ],
+    },
+      {
+      path: "/testing",
+      element: <CommonLayout />,
+      children: [
+        {
+          path: "",
+          element: (
+            <TestAPIConnection />
           ),
         },
       ],
